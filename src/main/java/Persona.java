6b1d6c2fbc768fe452642a2a7bddd31b61fcaa42
tomygabutti <tomygabutti@gmail.com>
2021-08-08 17:16:02 -0300

    Preparando Todo

---
+++ src/main/java/Persona.java
@@ -0,0 +1,9 @@
+import java.util.Date;
+
+abstract class Persona {
+    private String nombre;
+    private String apellido;
+    private Date fechaDeNacimiento;
+    private String tipoDocumento;
+    private String nroDocumento;
+}
